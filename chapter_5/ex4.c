16573f145b3ac77895e9cc1ecf795725e0ad8959
cruisez <sooryaghanesh@gmail.com>
2023-04-13 21:01:00 +0530

    minor tweaks

--- chapter_5/ex4.c
+++ chapter_5/ex4.c
@@ -1,3 +1,18 @@
+/*Write a program that acts as a simple “printing” calculator. The program
+  should allow the user to type in expressions of the form
+  
+ 		number   operator
+ 
+  The following operators should be recognized by the program:
+ 
+ 		+     -     *     /     S     E
+ 
+  The S operator tells the program to set the “accumulator” to the typed-in
+  number. The E operator tells the program that execution is to end. The
+  arithmetic operations are performed on the contents of the accumulator with
+  the number that was keyed in acting as the second operand.
+*/
+
 #include <stdio.h>
 
 int main(void)
@@ -15,7 +30,7 @@ int main(void)
         switch (operator)
         {
         case 'S':
-            printf("= %f\n", accumulator += number);
+            printf("= %f\n", accumulator = number);
             break;
 
         case '+':
@@ -23,7 +38,7 @@ int main(void)
             break;
 
         case '-':
-            printf("= %f\n", accumulator -= number);
+            printf("%f\n", accumulator -= number);
             break;
 
         case '*':
@@ -37,6 +52,7 @@ int main(void)
 
             else
                 printf("= %f\n", accumulator /= number);
+            break;
         }
 
         default:
